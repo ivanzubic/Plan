c44525b0301c3c3c704510ba7002f6adc7c26937
ivanzubic <ivanzubic27@gmail.com>
2026-03-30 19:39:14 +0200

    Reset game

--- igra.py
+++ igra.py
@@ -3,6 +3,7 @@ import random
 
 pygame.init()
 
+
 WIDTH, HEIGHT = 500, 700
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("F1 Images")
@@ -37,36 +38,67 @@ font = pygame.font.SysFont(None, 36)
 scroll_y = 0
 started = False
 engine_playing = False
+game_over = False
 
 def draw():
     win.fill((34, 139, 34))
 
-    # Cesta
+#Cesta
     pygame.draw.rect(win, (50, 50, 50), (100, 0, 300, HEIGHT))
 
-    # Linije
+#Linije
     for i in range(-40, HEIGHT, 40):
         pygame.draw.rect(win, (255, 255, 255), (240, i + scroll_y, 20, 20))
 
-    # Igrač (SLIKA)
+#Igrac
     win.blit(player_img, (player.x, player.y))
 
-    # Protivnici (SLIKE)
+#Protivnik  
     for e in enemies:
         win.blit(enemy_img, (e.x, e.y))
 
-    # Score
+#Score
     text = font.render(f"Score: {score}", True, (255, 255, 255))
     win.blit(text, (10, 10))
 
     pygame.display.update()
 
+def reset_game():
+    global player, enemies, score, scroll_y, started
+
+    player.x = 250
+    player.y = 500
+
+    enemies.clear()
+
+    score = 0
+    scroll_y = 0
+    started = False
 def main():
-    global scroll_y, score, started, engine_playing
+    global scroll_y, score, started, engine_playing, game_over
     run = True
 
     while run:
         clock.tick(60)
+        if game_over:
+            win.fill((0, 0, 0))
+
+            text = font.render("GAME OVER - press R", True, (255, 0, 0))
+            win.blit(text, (120, 300))
+
+            pygame.display.update()
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_r]:
+                game_over = False
+                enemies.clear()
+                player.x = 250
+                player.y = 500
+                score = 0
+                scroll_y = 0
+                started = False
+                engine_playing = False
+            continue
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -92,20 +124,20 @@ def main():
         if scroll_y > 40:
             scroll_y = 0
 
-        # Spawn
+        #Spawn
         if random.randint(1, 25) == 1:
             x = random.randint(120, 340)
             enemies.append(pygame.Rect(x, -80, 40, 70))
 
-        # Kretanje
+        #Kretanje
         for e in enemies[:]:
             if started:
                 e.y += 6
 
             if player.colliderect(e):
-                print("GAME OVER")
-                pygame.quit()
-                return
+                reset_game()
+                #pygame.quit()
+                #return
 
             if e.y > HEIGHT:
                 enemies.remove(e)
